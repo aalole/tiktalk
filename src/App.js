d9a5358ddab5df3e3acefa9e20a0ecdec1d39009
MR-aalole <52821144+Temitope25@users.noreply.github.com>
2020-10-18 19:32:41 +0100

    ft/chat -completed chat functionality -ensures user can chat any member of his or her network

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import Login from "./components/Login";
+import MyNetwork from "./components/MyNetwork";
+import Messanger from "./components/Messanger"
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <nav className="navbar">
+            <ul className="navbar-list">
+              <li className="navbar-item">
+                <Link to="/mynetwork" className="navbar-link">
+                  My Network
+                </Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/chats" className="navbar-link">
+                  Chats
+                </Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/" className="navbar-link">
+                  Home
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <Route path="/" exact component={Login} />
+          <Route path="/mynetwork" component={MyNetwork} />
+          <Route path="/chats" component={Messanger}/>
+        </Router>
+      </div>
+    );
+  }
 }
-
 export default App;
+
---
+++ src/components/Users.js
@@ -0,0 +1,62 @@
+export const dummyUsers = [
+  {
+    id: "2",
+    name: "Tomisin Lalude",
+    email: "tomisinlalude@gmail.com",
+    photoUrl:
+      "https://res.cloudinary.com/ts-i/image/upload/v1596679590/mentor-photos/mentor7-tomisin_qyxwvz.png",
+    role: "Member",
+    info: "Frontend Developer at HNG",
+    welcomeMessage: "Hey there! Love to chat :-)",
+  },
+  {
+    id: "3",
+    name: "Seyi Onifade",
+    email: "seyi@hng.tech",
+    photoUrl:
+      "https://res.cloudinary.com/bellomuboye/image/upload/v1597267321/xyluz_cv7pzo.jpg",
+    role: "Member",
+    info: "CEO, HNG tech",
+    welcomeMessage: "Hey there! Love to chat :-)",
+  },
+  {
+    id: "4",
+    name: "Grace Loveday",
+    email: "grace@sample.com",
+    photoUrl: "https://randomuser.me/api/portraits/women/44.jpg",
+    role: "Member",
+    info: "Product Designer at Google",
+    welcomeMessage: "Hey there! Love to chat :-)",
+  },
+  {
+    id: "5",
+    name: "Angela Jobs",
+    email: "angela@sample.com",
+    photoUrl:
+      "https://pbs.twimg.com/profile_images/834493671785525249/XdLjsJX_.jpg",
+    role: "Member",
+    info: "Software Engineer at TalkJS",
+    welcomeMessage: "Hey there! Love to chat :-)",
+  },
+  {
+    id: "6",
+    name: "Motunrayo",
+    email: "motun@gmail.com",
+    photoUrl:
+      "https://res.cloudinary.com/ts-i/image/upload/v1596679588/mentor-photos/mentor6-motun_gcbjjg.png",
+    role: "Member",
+    info: "Software Engineer at Linkedin",
+    welcomeMessage: "Hey there! Love to chat :-)",
+  },
+
+  {
+    id: "7",
+    name: "Favor Chibueze",
+    email: "cibuezefavor@sample.com",
+    photoUrl:
+      "https://res.cloudinary.com/bellomuboye/image/upload/v1597267724/phavvy_bet2jg.jpg",
+    role: "Member",
+    info: "Frontend Engineer at Hotels.ng",
+    welcomeMessage: "Hey there! Love to chat :-)",
+  },
+];
